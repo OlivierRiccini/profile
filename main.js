35b38dcaa6bbd45a85d6cddb3e36348f136861c6
Olivier Riccini <olivier-webdev@hotmail.com>
2018-02-07 17:03:16 -0500

    my profile page

---
+++ main.js
@@ -0,0 +1,13 @@
+$( "#btn1").click(function() {
+  $( "#player1" ).toggle( "slow");
+});
+
+$( "#btn2").click(function() {
+  $( "#player2" ).toggle( "slow");
+});
+
+$( "#btn3").click(function() {
+  $( "#player3" ).toggle( "slow");
+});
+
+
